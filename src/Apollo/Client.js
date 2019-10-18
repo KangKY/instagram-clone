89bff3087a3824992d5956918a54199cdfc6d9fa
Kang_KI <ruddlf4933@gmail.com>
2019-10-18 11:54:35 +0900

    mend

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "https://kistaclone.herokuapp.com/",
+  uri: "https://secondfamily.herokuapp.com/",
   //uri: "http://localhost:4000/",
   clientState: {
     defaults,
